6370327b2eb89f3e8ff94cfb24929b6052021d31
Juan Maxwell Noronha <juanmnoronha@gmail.com>
2024-08-02 20:03:55 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Natalia, eu amo muito você. Se você me ama, dá uma risadinha!</h1>
+    return <h1>Natalia, eu amo muito você. Se você me ama, dá um sorriso lindo!</h1>
 }
 
 export default Home;
