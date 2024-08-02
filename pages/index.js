fc4eab5ccc779fae1e2ec4ba38afaa9e38da2143
Juan Maxwell Noronha <juanmnoronha@gmail.com>
2024-08-02 20:25:44 +0000

    modifica a frase ultima vez

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Natalia, você é o amor da minha vida, se eu merecer, gostaria de um abraçø bem apertado!!!</h1>
+    return <h1>Natalia, eu te amo demais, se você me ama, dá uma piscadinha!</h1>
 }
 
 export default Home;
