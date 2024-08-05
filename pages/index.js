13b9647a1c5bf950289913b6b764eaefaa558b37
Juan Maxwell Noronha <juanmnoronha@gmail.com>
2024-08-05 12:06:58 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Natalia, eu te amo demais, se você me ama, dá uma piscadinha!</h1>
+  return <h1>Natalia, eu te amo demais, se você me ama, dá uma piscadinha!</h1>;
 }
 
 export default Home;
